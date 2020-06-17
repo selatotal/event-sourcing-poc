1ed282c6cfc47856fcbadf63f2b40bf1e480184a
Tales Viegas <tales.viegas@ilegra.com>
2020-06-17 16:16:11 -0300

    Fix class name

--- services/classroom-service/src/main/java/br/com/selat/classroomservice/ClassroomServiceApplication.java
+++ services/classroom-service/src/main/java/br/com/selat/classroomservice/ClassroomServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class classroomServiceApplication {
+public class ClassroomServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(classroomServiceApplication.class, args);
+		SpringApplication.run(ClassroomServiceApplication.class, args);
 	}
 
 }
