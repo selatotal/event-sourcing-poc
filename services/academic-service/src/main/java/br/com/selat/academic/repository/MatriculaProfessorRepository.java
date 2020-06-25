b33233cb95edfe92ae8897be10d46cf30cc7e127
Tales Viegas <tales.viegas@ilegra.com>
2020-06-25 18:07:03 -0300

    MatriculaProfessor

--- services/academic-service/src/main/java/br/com/selat/academic/repository/MatriculaProfessorRepository.java
+++ services/academic-service/src/main/java/br/com/selat/academic/repository/MatriculaProfessorRepository.java
@@ -4,6 +4,9 @@ import br.com.selat.academic.model.MatriculaProfessor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MatriculaProfessorRepository extends CrudRepository<MatriculaProfessor, String> {
+    List<MatriculaProfessor> findAll();
 }
---
+++ services/academic-service/src/main/java/br/com/selat/academic/service/MatriculaProfessorService.java
@@ -0,0 +1,62 @@
+package br.com.selat.academic.service;
+
+import br.com.selat.academic.contract.v1.event.EventEntity;
+import br.com.selat.academic.contract.v1.event.EventType;
+import br.com.selat.academic.contract.v1.exception.NotFoundException;
+import br.com.selat.academic.model.MatriculaProfessor;
+import br.com.selat.academic.repository.MatriculaProfessorRepository;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class MatriculaProfessorService {
+
+    private static final String NOT_FOUND_MESSAGE = "MatriculaProfessor not found";
+    private static final Gson gson = new Gson();
+    private final MatriculaProfessorRepository matriculaProfessorRepository;
+    private final KafkaService kafkaService;
+
+    @Autowired
+    public MatriculaProfessorService(MatriculaProfessorRepository matriculaProfessorRepository,
+                                     KafkaService kafkaService){
+        this.matriculaProfessorRepository = matriculaProfessorRepository;
+        this.kafkaService = kafkaService;
+    }
+
+    public List<MatriculaProfessor> getAll(){
+        return matriculaProfessorRepository.findAll();
+    }
+
+    public MatriculaProfessor getById(String id) {
+        return matriculaProfessorRepository.findById(id).orElseThrow(() -> new NotFoundException(NOT_FOUND_MESSAGE));
+    }
+
+    @Transactional
+    public MatriculaProfessor create(MatriculaProfessor input) {
+        MatriculaProfessor aluno = matriculaProfessorRepository.save(input);
+        kafkaService.publishEvent(EventType.CREATE, EventEntity.ALUNO, gson.toJson(aluno));
+        return aluno;
+    }
+
+    @Transactional
+    public MatriculaProfessor update(String id, MatriculaProfessor input) {
+        MatriculaProfessor entity = matriculaProfessorRepository.findById(id).orElseThrow(() -> new NotFoundException(NOT_FOUND_MESSAGE));
+        entity.setCodigoProfessor(input.getCodigoProfessor());
+        entity.setTurmasDisciplina(input.getTurmasDisciplina());
+        MatriculaProfessor aluno = matriculaProfessorRepository.save(entity);
+        kafkaService.publishEvent(EventType.UPDATE, EventEntity.ALUNO, gson.toJson(aluno));
+        return aluno;
+    }
+
+    @Transactional
+    public void delete(String id) {
+        MatriculaProfessor entity = matriculaProfessorRepository.findById(id).orElseThrow(() -> new NotFoundException(NOT_FOUND_MESSAGE));
+        matriculaProfessorRepository.delete(entity);
+        kafkaService.publishEvent(EventType.DELETE, EventEntity.ALUNO, gson.toJson(entity));
+    }
+
+}
